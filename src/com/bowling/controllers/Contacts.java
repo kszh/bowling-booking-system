27814658e4ed1e9d3934f0f7238a62dcc01a027f
Kseniia <kseniia.zheliabina@gmail.com>
2015-12-14 02:19:23 +0200

    Small fix

--- src/com/bowling/controllers/Contacts.java
+++ src/com/bowling/controllers/Contacts.java
@@ -1,9 +1,5 @@
 package com.bowling.controllers;
 
-
-import com.bowling.models.User;
-import com.bowling.services.interfaces.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
